c8d3af33ee00ac5535a759378d8aae290f48c506
Vishu-phogat <108395193+Vishu-phogat@users.noreply.github.com>
2024-01-26 14:48:59 +0530

    Update newindex.js

--- newindex.js
+++ newindex.js
@@ -2,25 +2,25 @@
 
 // scroll sections
 
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-  // sections.forEach(sec => {
-  //
-  //   let top = window.scrollY;
-  //   let offset = sec.offsetTop;
-  //   let height = sec.offsetHeight;
-  //   let id = sec.getAttribute('id');
-  //
-  //   if(top >= offset && top < (offset+height)){
-  //     // active navbar links
-  //     navLinks.forEach(links => {
-  //       links.classList.remove('active');
-  //       documents.querySelector('header nav a[href*= " + id + "]').classList.add('active');
-  //     });
-  //   };
-  // });
+  sections.forEach(sec => {
+  
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+  
+    if(top >= offset && top < (offset+height)){
+      // active navbar links
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        documents.querySelector('header nav a[href*= " + id + "]').classList.add('active');
+      });
+    };
+  });
 
   // sticky header
   let header = document.querySelector('header');
